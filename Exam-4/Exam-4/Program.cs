71f29ba478e772a86fccdc32ae39ce427fa39965
ggerick <erick210205>
2018-11-23 17:55:58 -0800

    Grafo y árboles

---
+++ Exam-4/Exam-4/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Arbolesss
+{
+    public class Nodo
+    {
+        public string dato;
+        public Nodo hijo;
+        public Nodo hermano;
+
+        public Nodo()
+        {
+            dato = " ";
+            hijo = null;
+            hermano = null;
+
+        }
+    }
+
+    public class Arbol
+    {
+        public Nodo raiz;
+        public Nodo trabajo;
+        public int i = 0;
+        public int altura = 0, y = 0;
+
+        public Arbol()
+        {
+            raiz = new Nodo();
+        }
+
+        public Nodo Insertar(string dato, Nodo INodo)
+        {
+            //Si no hay nodo donde insertar, lo definimos como raiz
+            if (INodo == null)
+            {
+                raiz = new Nodo();
+                raiz.dato = dato;
+
+                //No hay hijo
+                raiz.hijo = null;
+
+                //No hay hermano
+                raiz.hermano = null;
+                y++;
+                return raiz;
+            }
+
+            //Verificamos si no tiene hijo
+            //Insertamos el dato como hijo
+            if (INodo.hijo == null)
+            {
+                Nodo temporal = new Nodo();
+                temporal.dato = dato;
+
+                //Conectamos el nuevo como hijo
+                INodo.hijo = temporal;
+                y++;
+                return temporal;
+
+            }
+            else//El nodo ya tiene un hijo y tenemos que insertarlo como hermano
+            {
+                trabajo = INodo.hijo;
+
+                //Avanzamos hasta el último hermano 
+                while (trabajo.hermano != null)
+                {
+                    trabajo = trabajo.hermano;
+                }
+                //Creamos un nodo temporal   
+                Nodo temporal = new Nodo();
+                temporal.dato = dato;
+
+                //Unimos el temporal al ultimo hermano
+                trabajo.hermano = temporal;
+                y++;
+                altura = (y - 2 / 2);
+                return temporal;
+            }
+        }
+
+        public void TransversaPreOrder(Nodo INodo)
+        {
+            if (INodo == null)
+                return;
+
+            //Me proceseo primero a mi? 
+            for (int n = 0; n < i; n++)
+                Console.Write(" ");
+
+            Console.WriteLine(INodo.dato);
+
+            //Lueo procesamos el hijo
+            if (INodo.hijo != null)
+            {
+                i++;
+                TransversaPreOrder(INodo.hijo);
+                i--;
+            }
+
+            //Si tiene hermanos, se procesan
+            if (INodo.hermano != null)
+                TransversaPreOrder(INodo.hermano);
+        }
+
+        public void TransversaPostOrden(Nodo INodo)
+        {
+            if (INodo == null)
+                return;
+
+            //Primero se procesa al hijo
+            if (INodo.hijo != null)
+            {
+                i++;
+                TransversaPostOrden(INodo.hijo);
+                i--;
+            }
+
+            //Si tengo hermanos, los procesamos
+            if (INodo.hermano != null)
+                TransversaPostOrden(INodo.hermano);
+
+            //Luego hacemos un self-process
+            for (int n = 0; n < i; n++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(INodo.dato);
+        }
+
+        public Nodo Buscar(string dato, Nodo INodo)
+        {
+            Nodo encontrado = null;
+            if (INodo == null)
+                return encontrado;
+            if (INodo.dato.CompareTo(dato) == 0)
+            {
+                encontrado = INodo;
+                return encontrado;
+            }
+
+            //Luegose procesa al hijo
+            if (INodo.hijo != null)
+            {
+                encontrado = Buscar(dato, INodo.hijo);
+
+                if (encontrado != null)
+                    return encontrado;
+            }
+
+            //Si tengo hermanos, se procesan
+            if (INodo.hermano != null)
+            {
+                encontrado = Buscar(dato, INodo.hermano);
+                if (encontrado != null)
+                    return encontrado;
+            }
+
+            return encontrado;
+        }
+
+        public int alturaMaxima(Nodo INodo)
+        {
+            if (INodo == null)
+                return 0;
+
+
+            int hermano = alturaMaxima(INodo.hermano);
+            int hijos = alturaMaxima(INodo.hijo);
+
+            if (hermano > hijos)
+                return hermano + 1;
+
+            else
+                return hijos + 1;
+
+        }
+
+        public void AArboles()//Insertar los nodos en cada arbol y impresion de éstos
+        {
+            Console.WriteLine("\t\tArbol 1\n");
+            Nodo raiz = Insertar("a", null);
+            Nodo nodoB = Insertar("b", raiz);
+            Insertar("c", raiz);
+            Nodo nodoD = Insertar("d", raiz);
+            Nodo nodoE = Insertar("e", nodoB);
+            Insertar("f", nodoB);
+            Nodo nodoG = Insertar("g", nodoB);
+            Insertar("k", nodoE);
+            Insertar("l", nodoE);
+            Insertar("m", nodoE);
+            Nodo nodoN = Insertar("n", nodoG);
+            Insertar("r", nodoN);
+            Insertar("s", nodoN);
+            Nodo nodoH = Insertar("h", nodoD);
+            Insertar("i", nodoD);
+            Nodo nodoJ = Insertar("j", nodoD);
+            Insertar("o", nodoH);
+            Insertar("p", nodoJ);
+            Insertar("q", nodoJ);
+            Console.WriteLine("\t\tPre-order\n");
+            TransversaPreOrder(raiz);
+            Console.WriteLine("\t\tPost-order\n");
+            TransversaPostOrden(raiz);
+
+            Console.WriteLine("\t\tArbol 2\n");
+            Nodo raiz2 = Insertar("a", null);
+            Nodo nodoBB = Insertar("b", raiz2);
+            Insertar("c", nodoBB);
+            Insertar("d", nodoBB);
+            Nodo nodoEE = Insertar("e", raiz2);
+            Nodo nodoFF = Insertar("f", nodoEE);
+            Insertar("g", nodoFF);
+            Insertar("h", nodoFF);
+            Console.WriteLine("\t\tPre-order\n");
+            TransversaPreOrder(raiz2);
+            Console.WriteLine("\t\tPost-order\n");
+            TransversaPostOrden(raiz2);
+
+
+        }
+    }
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Arbol arboles = new Arbol();
+            arboles.AArboles();
+        }
+    }
+}
---
+++ Examen4-grafo/Examen4-grafo/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen4_Grafos
+{
+    class Grafo
+    {
+
+        public int Vertices;
+        List<int>[] lista;
+        public Grafo(int valor) 
+        {
+            Vertices = valor; 
+            lista = new List<int>[valor]; 
+            for (int i = 0; i < valor; i++) 
+            {
+                lista[i] = new List<int>();
+            }
+        }
+
+        public void Agregar(int Indice, int Valor) 
+        {
+            lista[Indice].Add(Valor); 
+        }
+
+
+
+        public void DFS(int value, int Dato) // Deep First Search
+        {
+            List<string> arrive = new List<string>();
+            if (Dato == 1 || Dato == 2)
+            {
+                arrive.Add("Boston");
+                arrive.Add("Chicago");
+                arrive.Add("Denver");
+                arrive.Add("LA");
+                arrive.Add("New York");
+                arrive.Add("San Francisco");
+                arrive.Add("Miami");
+                arrive.Add("Atlanta");
+            }
+            if (Dato == 3)
+            {
+                arrive.Add("Atlanta");
+                arrive.Add("Chicago");
+                arrive.Add("San Francisco");
+                arrive.Add("Boston");
+                arrive.Add("Denver");
+                arrive.Add("LA");
+                arrive.Add("New York");
+                arrive.Add("Miami");
+            }
+            if (Dato == 4)
+            {
+                arrive.Add("Denver");
+                arrive.Add("Chicago");
+                arrive.Add("New York");
+                arrive.Add("Atlanta");
+                arrive.Add("San Francisco");
+                arrive.Add("Boston");
+                arrive.Add("LA");
+                arrive.Add("Miami");
+            }
+
+            bool[] Visita = new bool[Vertices]; //  arreglo boleano del tamaño de nuestros vertices 
+            Stack<int> stck = new Stack<int>(); // creamos una pila
+            Visita[value] = true; // el valor del arreglo para saber si visitamos la ciudad
+            stck.Push(value); // agrega el valor parametro a la pila
+            Console.WriteLine("La ruta más corta es: ");          
+            while (stck.Count != 0) // mientras la pila tenga elementos
+            {
+                value = stck.Pop(); // el valor se iguala al ultimo valor de la pila qe va a sacar
+                Console.Write("{0}-> ", arrive[value]); // agrega el valor                
+                foreach (int item in lista[value])
+                {
+                    if (!Visita[item]) 
+                    {
+                        Visita[item] = true; 
+                        stck.Push(item); 
+                    }
+                }
+            }
+          
+        }
+    }
+    class Exammm
+    {
+
+        public void Grafo()
+        {
+            Ciclo:
+            Console.Clear();
+            Console.WriteLine("Elige un destino:\n1.- Boston -> LA\n2.- New York -> San Francisco\n3.- Atlanta -> San Francisco\n4.- Denver -> New York");
+            int opc = int.Parse(Console.ReadLine());
+            if (opc == 1)
+            {
+                Grafo BostonLA = new Grafo(5);
+                BostonLA.Agregar(0, 1);
+                BostonLA.Agregar(1, 2);
+                BostonLA.Agregar(2, 3);
+                BostonLA.DFS(0, 1);
+                Console.ReadKey();
+                goto Ciclo;
+            }
+
+            if (opc == 2)
+            {
+                Grafo NYSF = new Grafo(7);
+                NYSF.Agregar(0, 1);
+                NYSF.Agregar(1, 2);
+                NYSF.Agregar(2, 3);
+                NYSF.Agregar(4, 5);
+                NYSF.DFS(4, 2);
+                Console.ReadKey();
+                goto Ciclo;
+            }
+
+            if (opc == 3)
+            {
+                Grafo AtlSF = new Grafo(4);
+                AtlSF.Agregar(0, 1);
+                AtlSF.Agregar(1, 2);
+                AtlSF.DFS(0, 3);
+                Console.ReadKey();
+                goto Ciclo;
+            }
+
+            if (opc == 4)
+            {
+                Grafo DnverNY = new Grafo(4);
+                DnverNY.Agregar(0, 1);
+                DnverNY.Agregar(1, 2);
+                DnverNY.DFS(0, 4);
+              
+                Console.ReadKey();
+                goto Ciclo;
+            }
+
+
+
+        }
+
+
+
+
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Exammm grafo = new Exammm();
+            grafo.Grafo();
+
+
+        }
+  
+        }
+    }
